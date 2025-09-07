19c6ca35769b023978c1469d3003c4ad2750c3c1
prantikc22 <prantikchatterjee22@gmail.com>
2025-09-07 21:22:08 +0530

    allowed host added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  preview: {
+    allowedHosts: ['orbis.logicwerk.com'],
+  },
   server: {
     host: "::",
     port: 8080,
